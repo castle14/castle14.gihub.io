4ed85c0191e88e85586757fbca8ba088253b47d6
castle14 <617843579@qq.com>
2017-07-12 22:12:48 +0800

    add namecard

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$("#div_to_tomato").on("click",function(){
 		location.href="Tomato/html/2.html"
 	});
+	$("#div_to_namecard").on("click",function(){
+		location.href="tel.html"
+	});
 });
