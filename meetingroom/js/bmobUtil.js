d3840260cbdd1eaf4819ef1b32258fadf712c721
castle14 <617843579@qq.com>
2019-11-24 21:47:14 +0800

    优化了一下会议室查询的页面，但是还未完成。

---
+++ meetingroom/js/bmobUtil.js
@@ -0,0 +1,8 @@
+
+Bmob.initialize("081365fff35cb453", "123456");
+function getDate(){
+	var day2 = new Date();
+	day2.setTime(day2.getTime());
+	var s2 = day2.getFullYear()+"-" + (day2.getMonth()+1) + "-" + day2.getDate();
+	return s2;
+}
